f1f856ee442031d735164edcd5c3dbb8bb2a64b9
Rupesh Patil <patilrd861@gmail.com>
2021-12-30 19:00:41 +0530

    basic array and string reverse

---
+++ src/arrays/BasicArray.java
@@ -0,0 +1,71 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class BasicArray {
+	
+	public static void main(String[] args) {
+		MyArray myArray = new MyArray(2);
+		printArray(myArray);
+		myArray.add("Hi");
+		myArray.add("Hello");
+		printArray(myArray);
+		myArray.delete(0);
+		printArray(myArray);
+		myArray.add("Hi");
+		printArray(myArray);
+	}
+	
+	private static void printArray(MyArray myArray) {
+		System.out.println(Arrays.toString(myArray.getArray()) + "==data & lenght==" + myArray.length
+				+ " ==& capacity==" + myArray.capacity);
+	}
+
+}
+
+class MyArray {
+	
+	int capacity;
+	int length;
+	Object[] data;
+	
+	MyArray(){
+		this.length = 0;
+		this.capacity = 1;
+		this.data = new Object[capacity];
+	}
+	
+	MyArray(int capacity){
+		this.length = 0;
+		this.capacity = capacity;
+		this.data = new Object[capacity];
+	}
+	
+	public void add(Object ele) {
+		if(capacity>0) {
+			data[length] = ele;	
+			length++;
+			capacity--;
+		} else {
+			throw new RuntimeException("Array is full");
+		}
+	}
+	
+	public void delete(int index) {
+		//Object deletedEle = this.data[index];
+		for(int i=index; i < length-1; i++) {
+			this.data[i] = this.data[i+1];
+		}
+		this.data[length-1] = null;
+		length--;
+		capacity++;
+	}
+	
+	public Object[] getArray() {
+		Object[] tempArray = new Object[length];
+		for(int i=0; i < length; i++) {
+			tempArray[i] = data[i];
+		}
+		return tempArray;
+	}
+}
---
+++ src/arrays/ReverseString.java
@@ -0,0 +1,62 @@
+package arrays;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String str = "My Name is Rupesh";
+		System.out.println("Original String: " + str);
+		System.out.println("===========");
+		reverseString1(str);
+		System.out.println("===========");
+		reverseString2(str);
+		System.out.println("===========");
+		reverseWords(str);
+	}
+
+	private static void reverseString1(String str) {
+		if (str.length() < 2) {
+			System.out.println("String is less than 2 chars");
+			return;
+		}
+
+		char[] array = str.toCharArray();
+		int length = array.length;
+
+		char[] reverseArray = new char[length];
+
+		int x = length - 1;
+		for (int i = 0; i < array.length; i++) {
+			reverseArray[i] = array[x--];
+		}
+
+		System.out.println("Reversed String using method 1: " + new String(reverseArray));
+	}
+
+	private static void reverseString2(String str) {
+
+		if (str.length() < 2) {
+			System.out.println("String is less than 2 chars");
+			return;
+		}
+
+		String reverseString = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+			reverseString += str.charAt(i);
+		}
+		System.out.println("Reversed String using method 2: " + reverseString);
+	}
+	
+	private static void reverseWords(String str) {
+		String[] strArray = str.split(" ");
+		if (strArray.length < 2) {
+			System.out.println("String is less than 2 words");
+			return;
+		}
+		String result = "";
+		for (int i = strArray.length - 1; i >= 0; i--) {
+			result += strArray[i]+" ";
+		}
+		System.out.println("Reversed Words: " + result);
+	}
+
+}
